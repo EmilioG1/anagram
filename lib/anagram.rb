a688e0308923af0ab94986cbec4a27effa4620d5
Emilio Gonzales <emiliog1184@gmail.com>
2022-05-06 16:53:48 -0700

    final push to clean up code

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -43,10 +43,3 @@ class Word
     @result
   end
 end
-
-    
-#     # check if two words have exact same letters but in different order
-#     # make sure to downcase()
-#     # check to make sure inputted words have vowels, return a message if there are no vowels ('not an actual word!')
-#     # check if they are 'antigrams' -- none of the letters match -- and return a message
-#     # check for sentences as well -- MUST REMOVE spaces and puncuation ... maybe a regex for punctuation?...
