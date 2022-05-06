a147ff2a43124c6cccfe3707f84129d33e00d46b
Emilio Gonzales <emiliog1184@gmail.com>
2022-05-06 15:58:15 -0700

    add ability to remove punctuation '

--- script.rb
+++ script.rb
@@ -3,10 +3,10 @@ require('./lib/anagram')
 
 puts 'Welcome to the Anagram-a-game, where you will enter two words to see if they are anagrams of each other'
 puts 'Please enter the first word'
-input1 = gets.chomp
+input1 = gets.chomp.sub(/[!@#$%^&*()-=_+|;':",.<>?']/, '')
 puts "You wrote #{input1}"
 puts 'Please enter the second word'
-input2 = gets.chomp
+input2 = gets.chomp.gsub(/[!@#$%^&*()-=_+|;':",.<>?']/, '')
 puts "you wrote #{input2}"
 gram = Word.new(input1, input2)
 puts "The result is... #{gram.anagram}"
