985694c6fbe2e4d3f93e11a14057a48cbc748b27
Emilio Gonzales <emiliog1184@gmail.com>
2022-05-06 08:37:23 -0700

    test passed

--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -3,8 +3,14 @@ require('pry')
 require('anagram')
 
 describe('Word#anagram') do
-  it('returns true passed two letters') do
+  it('returns true if passed two letters') do
     x = Word.new('a', 'b').anagram
+    puts x
+    expect(x).to(eq(true))
+  end
+
+  it('will lowercase uppercased letters and return true') do
+    x = Word.new('B', 'N').anagram
     puts x
     expect(x).to(eq(true))
   end
